a0e78fab2b559f750fbbc7584976d717c1a99683
Lipsitapatel/python My Captain Assignment <88341717+Lipsitapatel@users.noreply.github.com>
2021-08-27 16:35:06 +0530

    Create My Assiengment 2 Fibonacci.py

---
+++ My Assiengment 2 Fibonacci.py
@@ -0,0 +1,24 @@
+
+
+
+ n = int(input("Enter the number : "))
+def fib(n):   
+   
+     
+  if n < 0:   
+    
+   print(" Print incorrect input ")
+   #First Fibonacci number is 0
+
+  elif n == 1:       
+              return 0
+   #Second Fibonacci number is 1
+
+  elif n == 2:        
+              return 1    
+  else:        
+        return (n-1) + (n+2)
+
+       #Driver program
+
+        print (n(9))
