a740b790e21450ece6abec9c278ef6caebecf4f6
Lipsitapatel/python My Captain Assignment <88341717+Lipsitapatel@users.noreply.github.com>
2021-08-27 17:20:22 +0530

    Create 2ns Assiengment postive_number.py

---
+++ 2ns Assiengment postive_number.py
@@ -0,0 +1,14 @@
+list1 = [12,-75, 64,-14] 
+  
+
+for i in list1:
+   if i > 0:
+    print (i)
+
+ list2 =[ 12,14,-95, 3] 
+
+for i in list2:
+     if i <=0:
+
+       list2.remove(i) 
+print (list2)
